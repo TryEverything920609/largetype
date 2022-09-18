c31f4b40044417bcd1156003d5cad808467b904e
tryeverything920609 <josh@raineragency.com>
2022-09-18 13:48:09 +0700

    Add controls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { FC, useEffect, useRef, useState } from 'react'
@@ -59,6 +60,7 @@ interface TextDisplay {
 const TextDisplay: FC<TextDisplay> = (props) => {
   const container = useRef<HTMLDivElement>(null)
   const content = useRef<HTMLDivElement>(null)
+  const [showControls, setShowControls] = useState(false)
   useEffect(() => {
     autofit(container.current!, content.current!)
   }, [props.text])
@@ -87,10 +89,34 @@ const TextDisplay: FC<TextDisplay> = (props) => {
     <div
       id="text-output"
       className="fixed inset-0 bg-black overflow-hidden flex items-center justify-center text-center"
+      onClick={() => setShowControls((x) => !x)}
       ref={container}
     >
       <div ref={content}>
         <div className="p-[0.5em]">{props.text}</div>
+      </div>
+      <div
+        className={clsx(
+          'absolute top-4 right-4',
+          showControls ? '' : 'opacity-0 pointer-events-none',
+        )}
+      >
+        <button className="btn btn-circle btn-outline" onClick={onExit}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   )
